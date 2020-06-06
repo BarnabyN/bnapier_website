5d3b69ca14e699b01d8a34768ae405ccda852e63
Barnaby Napier <barnabynapier@gmail.com>
2020-06-06 15:48:44 +0100

    Teardown to build from scratch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
-
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-
-import BlogPage from "./pages/BlogPage";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <p>In here you put your DOM stuff</p>
-      <p>Second line</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
