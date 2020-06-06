40c7c80a1800e9c9c5e7e3213eb349775c9ff12f
Barnaby Napier <barnabynapier@gmail.com>
2020-06-06 15:34:19 +0100

    Teardown to build from scratch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+
+import BlogPage from "./pages/BlogPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>In here you put your DOM stuff</p>
+      <p>Second line</p>
     </div>
   );
 }
---
+++ frontend/src/pages/BlogPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function BlogPage() {
+  const a = "a";
+  return (
+    <div>
+      <p>This</p>
+      <p>That</p>
+    </div>
+  );
+}
